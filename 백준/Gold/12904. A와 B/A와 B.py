07f23f270d2427b773fa02d74ecef523b5371893
JIMIN <84549242+yujimin413@users.noreply.github.com>
2026-03-04 21:23:47 +0900

    [Gold V] Title: A와 B, Time: 60 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -5,12 +5,29 @@ from collections import deque
 s = input().strip()
 t = input().strip()
 
-# t에서 역순으로 s 찾기
-# 1. 문자열 뒤에서 A 제거
-# 2. 문자열 뒤에서 B 제거하고 문자열 뒤집기
-while len(t) > len(s):
-    if t[-1] == 'A':
-        t = t[:-1] # 'A' 제거
-    else: # t[-1] == 'B'
-        t = t[:-1][::-1] # 'B'제거 후 뒤집기
-print(1 if s == t else 0)
+q = deque([t])
+visited = {t} # set([t]) 이미 방문한 문자열 저장
+flag = False
+while q:
+    cur = q.popleft()
+    
+    if cur == s:
+        flag = True
+        break
+    
+    if len(cur) < len(s):
+        continue
+    
+    # 역연산1: 끝이 A면 A 제거
+    if cur[-1] == 'A':
+        nxt = cur[:-1]
+        if nxt not in visited:
+            visited.add(nxt)
+            q.append(nxt)
+    elif cur[-1] == 'B':
+        nxt = cur[:-1][::-1]
+        if nxt not in visited:
+            visited.add(nxt)
+            q.append(nxt)
+
+print(1 if flag else 0)
