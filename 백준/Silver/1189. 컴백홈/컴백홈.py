093cd83d9c92903a92f19a3d0b288751d82c2ad6
JIMIN <84549242+yujimin413@users.noreply.github.com>
2026-03-08 16:12:49 +0900

    [Silver I] Title: 컴백홈, Time: 136 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.py
+++ 백준/Silver/1189. 컴백홈/컴백홈.py
@@ -2,28 +2,28 @@ import sys
 input = sys.stdin.readline
 
 r, c, k = map(int, input().split())
-grid = [[] * c  for _ in range(r)]
+grid = [list(input().strip()) for _ in range(r)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-for i in range(r):
-    grid[i] = list(input().strip())
-
 visited = [[False] * c for _ in range(r)]
-res = 0; # k인 가짓수
+res = 0 # k인 가짓수
 
 def dfs(cx, cy, cdist):
     global res
-    if cx == 0 and cy == c - 1 and cdist == k:
-        res += 1
+    
+    if cdist == k: 
+        if cx == 0 and cy == c - 1:
+            res += 1
         return
         
     for i in range(4):
         nx, ny = cx + dx[i], cy + dy[i]
         if 0 <= nx < r and 0 <= ny < c and grid[nx][ny] != 'T' and not visited[nx][ny]:
-            visited[cx][cy] = True
+            visited[nx][ny] = True
             dfs(nx, ny, cdist + 1)
-            visited[cx][cy] = False
+            visited[nx][ny] = False
 
+visited[r - 1][0] = True
 dfs(r - 1, 0, 1) # start
 print(res)
